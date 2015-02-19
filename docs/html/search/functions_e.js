eeb1a6483678e0d7f43247e13c87c8c48b8b1b04
Jeff Rose <jrose0@gmail.com>
2015-02-19 17:58:36 -0500

    Oops. Forgot to add a few files.

---
+++ docs/html/search/functions_e.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['toaxial',['ToAxial',['../structca_1_1axoninteractive_1_1_geometry_1_1_hex_grid_1_1_cubic_hex_coord.html#a6320df422e8823c104352e9af976e6b5',1,'ca.axoninteractive.Geometry.HexGrid.CubicHexCoord.ToAxial()'],['../structca_1_1axoninteractive_1_1_geometry_1_1_hex_grid_1_1_offset_hex_coord.html#a4ff61ed126d844c6a6285a907cd0c8d5',1,'ca.axoninteractive.Geometry.HexGrid.OffsetHexCoord.ToAxial()']]],
+  ['tocubic',['ToCubic',['../structca_1_1axoninteractive_1_1_geometry_1_1_hex_grid_1_1_axial_hex_coord.html#a093f4aaee720e181ab910aa08ada6aa5',1,'ca.axoninteractive.Geometry.HexGrid.AxialHexCoord.ToCubic()'],['../structca_1_1axoninteractive_1_1_geometry_1_1_hex_grid_1_1_offset_hex_coord.html#afc0aff9104b8a35bf6357cf610daddc7',1,'ca.axoninteractive.Geometry.HexGrid.OffsetHexCoord.ToCubic()']]],
+  ['tofloataxial',['ToFloatAxial',['../structca_1_1axoninteractive_1_1_geometry_1_1_hex_grid_1_1_float_cubic.html#a74425ca78a2309bbd10e47122b7eb98f',1,'ca::axoninteractive::Geometry::HexGrid::FloatCubic']]],
+  ['tofloatcubic',['ToFloatCubic',['../structca_1_1axoninteractive_1_1_geometry_1_1_hex_grid_1_1_float_axial.html#ab1b0e96916c693eeb7d6da3199b92556',1,'ca::axoninteractive::Geometry::HexGrid::FloatAxial']]],
+  ['tooffset',['ToOffset',['../structca_1_1axoninteractive_1_1_geometry_1_1_hex_grid_1_1_axial_hex_coord.html#a8292a2893006202bf17af8f01af61bbb',1,'ca.axoninteractive.Geometry.HexGrid.AxialHexCoord.ToOffset()'],['../structca_1_1axoninteractive_1_1_geometry_1_1_hex_grid_1_1_cubic_hex_coord.html#a09721b366bcd36890d112970314d3e75',1,'ca.axoninteractive.Geometry.HexGrid.CubicHexCoord.ToOffset()']]]
+];
---
+++ docs/html/search/functions_f.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['vec2d',['Vec2D',['../structca_1_1axoninteractive_1_1_geometry_1_1_hex_grid_1_1_vec2_d.html#ad70e842acd9176aa4d8a562f49e56603',1,'ca::axoninteractive::Geometry::HexGrid::Vec2D']]]
+];
